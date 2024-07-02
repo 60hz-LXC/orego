986ac1d68865ec7313f0caa39265a76fda54440d
60hz-LXC <lachlancameronn@gmail.com>
2024-07-02 09:27:34 +0200

    upadted next.config.js to include env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+  },
+};
 
-module.exports = {
-    reactStrictMode: true,
-  };
+module.exports = nextConfig;
